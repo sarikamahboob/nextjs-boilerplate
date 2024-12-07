11b02fdb55bd4279e8913f0c923ec3e163503efd
sarikamahboob <sarika.mahboob1234@gmail.com>
2024-12-07 17:35:34 +0600

    feat: route handlers added

---
+++ nextjs-route-handlers/app/time/route.ts
@@ -0,0 +1,8 @@
+export const dynamic = "force-dynamic";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  return Response.json({
+    time: new Date().toLocaleTimeString(),
+  })
+}
